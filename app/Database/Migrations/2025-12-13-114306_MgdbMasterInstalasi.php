c94740c655dc141a9744723a846dba38eb40e921
chanpanut27 <chanpanut27@gmail.com>
2025-12-13 18:52:35 +0700

    commit next

---
+++ app/Database/Migrations/2025-12-13-114306_MgdbMasterInstalasi.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class MgdbMasterInstalasi extends Migration
+{
+    public function up()
+    {
+         $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'kode_instalasi' => [
+                'type'       => 'CHAR',
+                'constraint' => '10',
+            ],
+            'nama_instalasi' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'is_active' => [
+                'type'  => 'BOOLEAN',
+                'default' => 1
+            ],
+            'deleted' => [
+                'type'  => 'INT',
+                'default' => 0
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'default' => 'current_timestamp'
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'default' => 'current_timestamp on update current_timestamp'
+            ],
+            'created_by'     => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100'
+            ],
+            'updated_by'     => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100'
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'default' => 'current_timestamp'
+            ],
+            'deleted_by'     => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100'
+            ]
+         ]);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('master_instalasi');
+    }
+}
