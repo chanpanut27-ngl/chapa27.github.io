bcab378b169fde6e5e04c473497bd70a089c2c7d
chanpanut27 <chanpanut27@gmail.com>
2025-12-13 19:07:53 +0700

    commit next

--- app/Views/Backend/Master/Instalasi/_data.php
+++ app/Views/Backend/Master/Instalasi/_data.php
@@ -21,11 +21,11 @@
                 <td><?= $row['is_active'] == 1 ? '<span class="badge bg-success">Aktif</span>' : '<span class="badge bg-secondary">Tidak aktif</span>'; ?></td>
                 <td>
                     <div class="d-flex justify-content-start gap-1">
-                        <button type="button" class="btn btn-warning btn-sm" onclick="editData(<?= $row['id']; ?>)" title="Edit data">
-                            <i class="fa-solid fa-edit"></i>
+                        <button type="button" class="btn btn-warning rounded btn-sm" onclick="editData(<?= $row['id']; ?>)" title="Edit data">
+                            <span class="fa-solid fa-edit"></span>
                         </button>
-                        <button type="button" class="btn btn-danger btn-sm" onclick="deleteData(<?= $row['id']; ?>)" title="Hapus data">
-                            <i class="fa-solid fa-trash-alt"></i>
+                        <button type="button" class="btn btn-danger rounded btn-sm" onclick="deleteData(<?= $row['id']; ?>)" title="Hapus data">
+                            <span class="fa-solid fa-trash-alt"></span>
                         </button>
                     </div>
                 </td>
