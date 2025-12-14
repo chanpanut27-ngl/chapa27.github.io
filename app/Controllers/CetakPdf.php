0669bf8cf36559de20fa35cd53105c129e487b95
chanpanut27 <chanpanut27@gmail.com>
2025-12-15 02:28:04 +0700

    commit next

--- app/Controllers/CetakPdf.php
+++ app/Controllers/CetakPdf.php
@@ -3,11 +3,13 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Libraries\CustomLib;
 
 class CetakPdf extends BaseController
 {
     public function index()
     {
-        
+        $a = new CustomLib();
+       echo $a->greet('chan');
     }
 }
