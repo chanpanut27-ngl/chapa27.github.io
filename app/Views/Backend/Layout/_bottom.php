d103457705cf997ed303f5b1a12b974831a405c7
chanpanut27 <chanpanut27@gmail.com>
2025-12-15 03:31:35 +0700

    commit next

--- app/Views/Backend/Layout/_bottom.php
+++ app/Views/Backend/Layout/_bottom.php
@@ -7,13 +7,7 @@
 <script src="<?= base_url('assets/js/plugins/feather.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/fontawesome.v6.3.0.all.js'); ?>"></script>
 <script src="<?= base_url('assets/js/jquery-3.7.1.js'); ?>"></script>
-<script>
-    $(document).ready(function () {
-        const search = $(".dt-search #dt-search");
-        const place = search.setAttribute('placeholder');
-        place.val('xx');
-    })
-</script>
+
 <!-- [bottomAssets] start -->
 <?= $this->renderSection('bottomAssets'); ?>
 <!-- [bottomAssets] end -->
