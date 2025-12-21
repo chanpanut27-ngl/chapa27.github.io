232ee9e876b4e1df475d604bc0b194674864747e
chanpanut27 <chanpanut27@gmail.com>
2025-12-21 15:21:31 +0700

    commit next

---
+++ app/Views/Auth/Layout/footer.php
@@ -0,0 +1,10 @@
+<div class="auth-footer row">
+<!-- <div class=""> -->
+    <div class="col my-1">
+        <p class="m-0">Copyright © <a href="#">Program Layanan</a></p>
+    </div>
+    <div class="col-auto my-1">
+        <?= date('Y') ?>
+    </div>
+<!-- </div> -->
+</div>
