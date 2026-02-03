0a423212f00c8075e892455685d829160d9524e5
chanpanut27 <chanpanut27@gmail.com>
2026-02-04 02:51:17 +0700

    gogogogo

--- index1.php
+++ index1.php
@@ -48,7 +48,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // LOAD OUR PATHS CONFIG FILE
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../prola.bblkmjakarta.org/app/Config/Paths.php';
+require FCPATH . '../prolas.bblkmjakarta.org/app/Config/Paths.php';
 // ^^^ Change this line if you move your application folder
 
 $paths = new Paths();
