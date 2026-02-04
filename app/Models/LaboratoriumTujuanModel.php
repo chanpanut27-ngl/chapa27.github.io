93d9bfcddaa499ea9f6ea8ff1746157ee5d80e6c
chanpanut27 <chanpanut27@gmail.com>
2026-02-04 07:43:40 +0700

    ci4nows

--- app/Models/LaboratoriumTujuanModel.php
+++ app/Models/LaboratoriumTujuanModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use CodeIgniter\I18n\Time;
 use CodeIgniter\Model;
 
 class LaboratoriumTujuanModel extends Model
@@ -48,6 +49,26 @@ class LaboratoriumTujuanModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    protected function setInsertBy(array $data)
+    {
+        $username = user()->username;
+        if ($username) {
+            $data['data']['created_by'] = $username;
+        }
+        return $data;
+    }
+ 
+    protected function setUpdatedBy(array $data)
+    {
+       $username = user()->username;
+       $myTime = new Time();
+        if ($username) {
+            $data['data']['updated_by'] = $username;
+            $data['data']['updated_at'] = $myTime->toDateTimeString();
+        }
+        return $data;
+    }
 
     public function get_data($params)
     {
