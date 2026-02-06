9347a585157e6a05da478c8fdce6fe33e1909290
chanpanut27 <chanpanut27@gmail.com>
2026-02-07 01:16:22 +0700

    ci4nows

--- app/Views/Auth/Layout/__bottom.php
+++ app/Views/Auth/Layout/__bottom.php
@@ -3,7 +3,6 @@
 <script src="<?= base_url('assets/js/plugins/simplebar.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/plugins/bootstrap.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/fonts/custom-font.js'); ?>"></script>
-<script src="<?= base_url('assets/js/fonts/custom-ant-icon.js'); ?>"></script>
 <script src="<?= base_url('assets/js/pcoded.js'); ?>"></script>
 <script src="<?= base_url('assets/js/plugins/feather.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/fontawesome.v6.3.0.all.js'); ?>"></script>
