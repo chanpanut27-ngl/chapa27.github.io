d5867b6fcdc3fcd75da8c223690b69b21d4c1faa
chanpanut27 <chanpanut27@gmail.com>
2026-02-07 04:18:15 +0700

    ci4nows

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+/* Error Page 404 */
+$routes->set404Override('App\Controllers\ErrorPage::show404');
+
+$routes->get('not-privilege', 'NotEnoughPrivilege::show401');
+
 /* Admin */
 $routes->group('/', ['filter' => 'role:admin'], function ($routes) {
     $routes->get('', 'Admin::index');
---
+++ app/Controllers/ErrorPage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class ErrorPage extends BaseController
+{
+    protected $title;
+
+    protected $message;
+
+    public function __construct()
+    {
+        $this->title = lang('Errors.pageNotFound');
+        $this->message = 'Halaman tidak ditemukan';
+    }
+
+    public function show404()
+    {
+       
+        $data = [
+            'title' => $this->title,
+            'message' => $this->message
+        ];
+        return view('errors/html/error_404', $data);
+    }
+}
