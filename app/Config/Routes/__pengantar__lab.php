aafe07a3a7295af5dd105c10c3cbd25fb1446510
chanpanut27 <chanpanut27@gmail.com>
2026-02-09 02:47:44 +0700

    ci4nows

---
+++ app/Config/Routes/__pengantar__lab.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Pengantar LHU */
+$routes->group('pelayanan-pemeriksaan/pengantar-lab', function ($routes) {
+    $routes->get('', 'PengantarLab::index');
+    $routes->get('list-data', 'PengantarLab::list');
+    $routes->get('add-data', 'PengantarLab::new');
+    $routes->post('create-data', 'PengantarLab::create');
+    $routes->get('edit-data/(:num)', 'PengantarLab::edit/$1');
+    $routes->post('update-data', 'PengantarLab::update');
+    $routes->delete('delete-data/(:num)', 'PengantarLab::delete/$1');
+    $routes->get('setting-lab/(:num)', 'PengantarLab::setting_lab/$1');
+    $routes->post('create-setting-lab', 'PengantarLab::create_setting_lab');
+});
+?>
