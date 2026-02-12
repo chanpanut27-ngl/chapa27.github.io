49bb0964ee2b67340ac833a620265beb5462d319
chanpanut27 <chanpanut27@gmail.com>
2026-02-12 17:26:59 +0700

    ci4nows

---
+++ app/Config/Routes/__perintah__uji__sampel.php
@@ -0,0 +1,11 @@
+<?php
+/** Perintah uji sampel **/
+$routes->group('pelayanan/perintah-uji-sampel', function ($routes) {
+    $routes->get('', 'PerintahUjiSampel::index');
+    $routes->get('list-data', 'PerintahUjiSampel::list');
+    $routes->get('add-data', 'PerintahUjiSampel::new');
+    $routes->post('create-data', 'PerintahUjiSampel::create');
+    $routes->get('edit-data', 'PerintahUjiSampel::edit');
+    $routes->post('update-data', 'PerintahUjiSampel::update');
+    $routes->get('delete-data', 'PerintahUjiSampel::delete');
+});
