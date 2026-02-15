ab57af2fc3c9331b09f6b84abdee4d860e15f085
chanpanut27 <chanpanut27@gmail.com>
2026-02-16 03:13:47 +0700

    ci4nows

--- app/Views/Backend/Modul/Pelayanan/Permintaan/__data.php
+++ app/Views/Backend/Modul/Pelayanan/Permintaan/__data.php
@@ -1,7 +1,7 @@
 <table id="example" class="table table-hover table-bordered">
     <thead>
         <?php
-        $arrth = ['No', 'No.Registrasi', 'Nama', 'No.Telp', 'Instansi', 'Tgl & jam', ''];
+        $arrth = ['No', 'No.Registrasi', 'Nama', 'No.Telp/Hp pengirim', 'Instansi', 'No.Telp/Hp instansi', 'Tgl & jam', ''];
         echo '<tr>';
         foreach ($arrth as $th) :
             echo '<th>' . $th . '</th>';
@@ -14,12 +14,13 @@
         $no = 1;
         foreach ($items as $row) :
         ?>
-            <tr id="myId-<?= $row['id']; ?>" data-urut=<?= $no; ?>>
+            <tr id="myId-<?= $row['id'] ?>" data-urut=<?= $no; ?>>
                 <td><b><?= $no++; ?></b></td>
-                <td><?= $row['no_reg']; ?></td>
-                <td><?= $row['nama_pengirim']; ?></td>
-                <td><?= $row['no_telp']; ?></td>
-                <td><?= $row['instansi']; ?></td>
+                <td><?= $row['no_reg'] ?></td>
+                <td><?= $row['nama_pengirim'] ?></td>
+                <td><?= $row['no_telp_pengirim'] ?></td>
+                <td><?= $row['instansi'] ?></td>
+                <td><?= $row['no_telp'] ?></td>
                 <td><?= date('d-m-Y H:i', strtotime($row['created_at'])); ?></td>
                 <td>
                     <div class="d-flex justify-content-start gap-1">
