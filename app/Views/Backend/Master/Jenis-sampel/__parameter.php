5c37742630cfa96b40b64b808449ee8705840680
chanpanut27 <chanpanut27@gmail.com>
2026-02-16 17:19:33 +0700

    ci4nows

--- app/Views/Backend/Master/Jenis-sampel/__parameter.php
+++ app/Views/Backend/Master/Jenis-sampel/__parameter.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title" id="exampleModalLabel"><i class="ti ti-file"></i> <?= $title; ?></h3>
+                <h3 class="modal-title" id="exampleModalLabel"><span class="fa-solid fa-clipboard"></span> <?= $title; ?></h3>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
