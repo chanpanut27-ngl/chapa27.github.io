4a544f29258f391c6fdaef6bb51d98ab788446cd
chanpanut27 <chanpanut27@gmail.com>
2026-02-19 23:25:15 +0700

    ci4nows

--- app/Controllers/ExportController.php
+++ app/Controllers/ExportController.php
@@ -44,8 +44,6 @@ class ExportController extends BaseController {
 
     public function xls_instalasi() 
     {
-        // require '../prola.bblkmjakarta.org/vendor/autoload.php';
-        require '/prola.bblkmjakarta.org/vendor/autoload.php';
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
 
