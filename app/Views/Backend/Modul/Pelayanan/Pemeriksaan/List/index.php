c8ff5e8d868ae573e23780f526bd76708efc9787
chanpanut27 <chanpanut27@gmail.com>
2026-02-19 23:49:40 +0700

    ci4nows

--- app/Views/Backend/Modul/Pelayanan/Pemeriksaan/List/index.php
+++ app/Views/Backend/Modul/Pelayanan/Pemeriksaan/List/index.php
@@ -33,7 +33,7 @@
                 <div class="card">
                     <div class="card-header p-2">
                             <div class="d-flex justify-content-end align-items-center gap-1">
-                                <a href="<?= base_url('pelanggan/permintaan-pemeriksaan') ?>" class="btn btn-success btn-sm btn-rounded" title="Kembali"><span class="fa-solid fa-arrow-circle-left"></span></a>
+                                <a href="<?= base_url('pelayanan/permintaan-pelanggan') ?>" class="btn btn-success btn-sm btn-rounded" title="Kembali"><span class="fa-solid fa-arrow-circle-left"></span></a>
                                 <button type="button" class="btn btn-secondary btn-sm rounded btn-refresh-data">
                                     <span class="pc-micon"><span class="fa-solid fa-refresh"></span></span>
                                 </button>
@@ -54,28 +54,32 @@
                                     <div class="row">
                                         <div class="col-sm-3"><b>No.Registrasi</b></div>
                                         <div class="col-sm-3">: <?= $items['no_reg'] ?></div>
-                                        <div class="col-sm-3"><b>No.Telp/Hp</b></div>
+                                        <div class="col-sm-3"><b>No.Telp/Hp Pengirim</b></div>
                                         <div class="col-sm-3">: <?= $items['no_telp_pengirim'] ?></div>
                                     </div>
                                     <div class="row">
                                         <div class="col-sm-3"><b>Nama pengirim</b></div>
                                         <div class="col-sm-3">: <?= $items['nama_pengirim'] ?></div>
-                                        <div class="col-sm-3"><b>Tgl & jam pengambilan sampel</b></div>
-                                        <div class="col-sm-3">: <?= date('d-m-Y', strtotime($items['tgl_ambil_sampel'])).' '.date('H:i', strtotime($items['jam_ambil_sampel'])) ?></div>
+                                        <div class="col-sm-3 d-none"><b>Tgl & jam pengambilan sampel</b></div>
+                                        <div class="col-sm-3 d-none">: <?= date('d-m-Y', strtotime($items['tgl_ambil_sampel'])).' '.date('H:i', strtotime($items['jam_ambil_sampel'])) ?></div>
+                                        <div class="col-sm-3"><b>Instansi</b></div>
+                                        <div class="col-sm-3">: <?= $items['instansi'] ?></div>
                                     </div>    
                                     <div class="row">
                                         <div class="col-sm-3"><b>Spesimen/sampel</b></div>
                                         <div class="col-sm-3">: <?= $items['spesimen_atau_sampel'] ?></div>
-                                        <div class="col-sm-3"><b>Lokasi pengambilan sampel/spesimen	</b></div>
-                                        <div class="col-sm-3">: <?= $items['lokasi_ambil_sampel'] ?></div>
+                                        <div class="col-sm-3 d-none"><b>Lokasi pengambilan sampel/spesimen	</b></div>
+                                        <div class="col-sm-3 d-none">: <?= $items['lokasi_ambil_sampel'] ?></div>
+                                        <div class="col-sm-3"><b>Alamat</b></div>
+                                        <div class="col-sm-3">: <?= $items['alamat'] ?></div>
                                     </div>  
                                     <div class="row">
-                                        <div class="col-sm-3"><b>Petugas pengambilan sampel</b></div>
-                                        <div class="col-sm-3">: <?= $items['petugas_ambil_sampel'] ?></div>
-                                        <div class="col-sm-3"><b>Keterangan tambahan</b></div>
-                                        <div class="col-sm-3">: <?= $items['keterangan_tambahan'] ?></div>
+                                        <div class="col-sm-3 d-none"><b>Petugas pengambilan sampel</b></div>
+                                        <div class="col-sm-3 d-none">: <?= $items['petugas_ambil_sampel'] ?></div>
+                                        <div class="col-sm-3 d-none"><b>Keterangan tambahan</b></div>
+                                        <div class="col-sm-3 d-none">: <?= $items['keterangan_tambahan'] ?></div>
                                     </div>   
-                                    <div class="row">
+                                    <div class="row d-none">
                                         <div class="col-sm-3"><b>Instansi</b></div>
                                         <div class="col-sm-9">: <?= $items['instansi'] ?></div>
                                     </div>                                
