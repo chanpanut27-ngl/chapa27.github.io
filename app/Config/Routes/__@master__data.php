12d5e237212baeeaee4eab14f3067fb302081059
chanpanut27-ngl <chanpanut27@gmail.com>
2026-02-21 18:13:13 +0700

    Cinows

--- app/Config/Routes/__@master__data.php
+++ app/Config/Routes/__@master__data.php
@@ -101,4 +101,17 @@
         $routes->get('detail-parameter/(:num)', 'JenisSampelMaster::show_parameter/$1');
     });
 
+    /* Per item sampel */
+    $routes->group('master-data/parameter', function ($routes) {
+        $routes->get('', 'ParameterMaster::index');
+        $routes->get('list-data', 'ParameterMaster::list');
+        $routes->get('add-data', 'ParameterMaster::new');
+        $routes->post('create-data', 'ParameterMaster::create');
+        $routes->get('edit-data/(:num)', 'ParameterMaster::edit/$1');
+        $routes->post('update-data', 'ParameterMaster::update');
+        $routes->post('list-sampel', 'ParameterMaster::list_sampel');
+        $routes->post('detail-sampel', 'ParameterMaster::detail_sampel');
+        $routes->delete('delete-data/(:num)', 'ParameterMaster::delete/$1');
+    });
+
 ?>
