65f38232ddb649e6754e6d9d1ea55306d6afac21
chanpanut27-ngl <chanpanut27@gmail.com>
2026-02-25 15:37:47 +0700

    ci4nows

--- app/Views/Pelanggan/Pemeriksaan/List/index.php
+++ app/Views/Pelanggan/Pemeriksaan/List/index.php
@@ -50,7 +50,7 @@
                     <div class="accordion accordion-flush accordion-color" id="accordionFlushExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
+                            <button class="accordion-button bg-green-100 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
                                 <h5><span class="pc-micon"><span class="fa-solid fa-user"></span> Data Pelanggan</h5>
                             </button>
                             </h2>
